70c2a6f8b8ee06d876007754865ca6ee69a7354b
judyshyu <judyshyu.office@gmail.com>
2018-07-10 22:18:04 +0800

    preview page-applications

---
+++ EcoLumina/app_content.php
@@ -0,0 +1,78 @@
+<?php // include_once("db.php");
+$page_title = '應用方案'; // for meta title
+include __DIR__.'/include/__page_head.php';
+include __DIR__.'/include/__page_script.php';
+?>
+<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+	<?php include __DIR__.'/include/__page_navbar.php'; ?>
+	<article id="app_content" class="w1200">
+		<section class="title_container cf">
+			<div class="fleft">
+				<h3>科技安防跨界應用解決方案</h3>
+				<ul class="breadcrumb cf">
+					<li class="fleft"><a href="">SMART HOME</a>&emsp;─&emsp;</li>
+					<li class="fleft"><a href="">HOME01</a></li>
+				</ul>
+			</div>
+			<ul class="share fright cf">
+				<li class="fleft"><a href="" class="share-icon fb"></a></li>
+				<li class="fleft"><a href="" class="share-icon twitter"></a></li>
+				<li class="fleft"><a href="" class="share-icon line"></a></li>
+				<li class="fleft"><a href="" class="share-icon wa"></a></li>
+			</ul>
+		</section>
+		<section class="main">
+			<img src="https://api.fnkr.net/testimg/1200x600/aaaaaa/FFF/?text=pending">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum amet facilis rem asperiores minus perferendis qui natus, exercitationem optio quam, quisquam iste temporibus, consectetur, error quod nobis dolorem. Excepturi, amet?</p>
+		</section>
+	</article>
+	<section class="w1200 txt-center">
+		<h3>其他相關應用</h3>
+		<hr>
+		<ul id="app_list" class="txt-white cf">
+			<li class="col33 fleft">
+				<figure style="background-image: url(img/app/01.jpg);"></figure>
+				<div class="container">
+					<h4>科技安防跨界應用解決方案</h4>
+					<a href="app_content.php">閱讀更多</a>
+					<ul class="cf">
+						<li class="col25 fleft"><a href="#" class="share-icon fb"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon twitter"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon line"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon wa"></a></li>
+					</ul>
+				</div>
+			</li>
+			<li class="col33 fleft">
+				<figure style="background-image: url(img/app/01.jpg);"></figure>
+				<div class="container">
+					<h4>科技安防跨界應用解決方案</h4>
+					<a href="app_content.php">閱讀更多</a>
+					<ul class="cf">
+						<li class="col25 fleft"><a href="#" class="share-icon fb"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon twitter"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon line"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon wa"></a></li>
+					</ul>
+				</div>
+			</li>
+			<li class="col33 fleft">
+				<figure style="background-image: url(img/app/01.jpg);"></figure>
+				<div class="container">
+					<h4>科技安防跨界應用解決方案</h4>
+					<a href="app_content.php">閱讀更多</a>
+					<ul class="cf">
+						<li class="col25 fleft"><a href="#" class="share-icon fb"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon twitter"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon line"></a></li>
+						<li class="col25 fleft"><a href="#" class="share-icon wa"></a></li>
+					</ul>
+				</div>
+			</li>
+		</ul>
+	</section>
+	<?php include __DIR__.'/include/__page_foot.php'; ?>
+</body>
+</html>
---
+++ EcoLumina/js/page.js
@@ -0,0 +1,10 @@
+$(function(){
+	// $(window).on('load scroll', function(){
+	// 	var scrollTop = $(window).scrollTop();
+	// 	if(scrollTop === 0){
+	// 		header.removeClass('fixed');
+	// 	}else{
+	// 		header.addClass('fixed');
+	// 	}
+	// });
+})
