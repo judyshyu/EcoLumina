ecc7761298278733df947ba29515dba77bb06f3e
judyshyu <judyshyu.office@gmail.com>
2018-07-10 22:37:15 +0800

    Revert "preview page-applications"
    This reverts commit 70c2a6f8b8ee06d876007754865ca6ee69a7354b.

--- EcoLumina/include/__page_script.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/main.css?ver=<?php echo filemtime('css/main.css'); ?>">
-
-<script src="lib/jquery-3.3.1.min.js?ver=<?php echo filemtime('lib/jquery-3.3.1.min.js'); ?>"></script>
-<script src="lib/scrollreveal.min.js?ver=<?php echo filemtime('lib/scrollreveal.min.js'); ?>"></script>
-<script src="lib/jquery.mCustomScrollbar.min.js?ver=<?php echo filemtime('lib/jquery.mCustomScrollbar.min.js'); ?>"></script>
-
-<script src="js/main.js?ver=<?php echo filemtime('js/main.js'); ?>"></script>
